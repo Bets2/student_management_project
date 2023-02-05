6c1cd4949f307c91c4e966aaaa30ecf5a2534a90
Bets2 <86775932+Bets2@users.noreply.github.com>
2023-02-05 15:05:49 +0200

    Update settings.py

--- customer_management_project/settings.py
+++ customer_management_project/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'customer_management',
         'USER': 'root',
-        'PASSWORD': 'AddisAbaba001!',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
